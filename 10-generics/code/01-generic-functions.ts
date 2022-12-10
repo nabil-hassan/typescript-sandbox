2476b5a84407fa5f21fd25e421f3674e09d3c6fa
Nabil Hassan <nabil.a.hassan@gmail.com>
2022-12-10 21:31:57 +0000

    ...

--- 10-generics/code/01-generic-functions.ts
+++ 10-generics/code/01-generic-functions.ts
@@ -1,3 +1,25 @@
 console.log("============================================================================================")
 console.log("                     Generics                                                               ")
 console.log("============================================================================================")
+
+// a function which simply returns any type of value it is given
+function returnTheValue<T>(source : T):T {
+    return source;
+}
+console.log(returnTheValue("Foo"));
+console.log(returnTheValue(true));
+
+// a function which returns a random value from an array
+function returnRandomElement<T>(source: Array<T>): T {
+    let rand: number = Math.floor(Math.random() * source.length);
+    return source[rand];
+}
+
+function returnRandomElementV2<T>(source: T[]): T {
+    let rand: number = Math.floor(Math.random() * source.length);
+    return source[rand];
+}
+
+console.log(returnRandomElement(["A", "B", "C", "D"]));
+console.log(returnRandomElementV2([1,2,3,4]));
+
